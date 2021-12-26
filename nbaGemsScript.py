d732406ebd30b0a124f0a23d67b0897ca51a78e1
Jordan Bradshaw <Jordan.Bradshaw.69@my.csun.edu>
2021-12-26 09:05:21 -0800

    Update to Blocks && Steals

--- nbaGemsScript.py
+++ nbaGemsScript.py
@@ -92,10 +92,12 @@ def printScore(playerLog):
             if (int(currentYahooLog['ST']) == 2 and int(currentYahooLog['BLK']) == 2):
                 retString += f"**{int(playerLog['ST'])} stl/** **{int(playerLog['BLK'])} blk/** "
                 return retString
+        if (int(currentYahooLog['ST']) != 0):
             if (int(currentYahooLog['ST']) < 3):
                 retString += f"{int(playerLog['ST'])} stl/ "
             else:
                 retString += f"**{int(playerLog['ST'])} stl/** "
+        if (int(currentYahooLog['BLK']) != 0):
             if (int(currentYahooLog['BLK']) < 3):
                 retString += f"{int(playerLog['BLK'])} blk/ "
             else:
